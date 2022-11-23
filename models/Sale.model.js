c2b4e47e5cb2dd936435cfd70879ec6a6f46b755
Matias Molina Bishop <molibi01@gmail.com>
2022-11-23 11:46:11 +0100

    modelo sales

---
+++ models/Sale.model.js
@@ -0,0 +1,41 @@
+const { Schema, model } = require("mongoose");
+
+const saleSchema = new Schema({
+    creator:{ type: Schema.Types.ObjectId, ref: 'User'}, 
+
+    city: {
+        type: String,
+        required: true
+    },
+
+    price: {
+        type:Number,
+        required:true
+     }, //ASK IF ITS NUMBER IS OK OR WE NEED TO SPECIFY INTEGER FLOAT ?
+
+    instruments: {
+        type: String,
+        required: true,
+        enum: ['DJ', 'Piano', 'Guitar', 'Violin', 'Drums', 'Saxophone', 'Flute', 'Cello',
+        'Clarinet', 'Trumpet', 'Harp', 'Ukelele', 'Electric Guitar', 'Banjo', 'Accordion', 'mircophone']
+    },
+    description:{
+        type:String,
+        required: true,
+    },
+    coordinate: 
+    {
+        type:Array,
+        default: []
+    }, 
+    photo:{
+        imageUrl:[String], //link to see images
+        required: true,
+    }
+  },{
+    timestamps: true
+})
+
+const Sale = model("Sale", saleSchema);
+
+module.exports = Sale;
