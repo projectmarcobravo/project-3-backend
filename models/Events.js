70ed0365d4a0c53f93b4f5188de8a8e03eda3be1
zzcugat <zzcugat@gmail.com>
2022-11-23 11:29:30 +0100

    Events model

---
+++ models/Events.js
@@ -0,0 +1,70 @@
+const { Schema, model } = require("mongoose");
+
+// TODO: Please make sure you edit the User model to whatever makes sense in this case
+const userSchema = new Schema(
+    {
+        creator: {
+            type: Schema.Types.ObjectId,
+            ref: 'User'
+        },
+
+        title: {
+            type: String,
+            required: [true, "Please introduce the title"],
+        },
+        typeOfEvent: {
+            type: String,
+            required: [true, "Type of event is required"],
+            enum: ["Party", "Concert", "Join a Band", "Hiring a Member", "Jamming"
+
+            ]
+        },
+
+        style: {
+            type: String,
+            enum: ['Dance", "Folk", "Bachata", "Rock", "Reggaeton", "Rap", "Flamenco", "Classic", "Tango", "Indie","Trap", "Pop", "Electronic", "Blues", "Punk", "Jazz", "Techno", "Choir", "Trance",]
+        },
+
+        instruments: {
+            type: String,
+            enum: ['DJ', 'Piano', 'Guitar', 'Violin', 'Drums', 'Saxophone', 'Flute', 'Cello',
+                'Clarinet', 'Trumpet', 'Harp', 'Ukelele', 'Electric Guitar', 'Banjo', 'Accordion', 'Microphone']
+        },
+
+        picture: {
+            type: String,
+        },
+
+        phoneNumber: {
+            type: String,
+        },
+
+        description: {
+            type: String,
+        },
+
+        price: {
+            type: Number,
+            required: true
+        },
+
+        date: {
+            type: Date,
+            required: [true, "Date is required."],
+        },
+
+        coordinate: {
+            type: Array,
+            default: []
+        },
+
+    },
+    {
+        // this second object adds extra properties: `createdAt` and `updatedAt`
+        timestamps: true,
+    }
+);
+
+const Event = model("Event", userSchema);
+
+module.exports = Event;
