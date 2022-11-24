6e9899f8de2743972066d1d1a70ac311b8b9238e
Matias Molina Bishop <molibi01@gmail.com>
2022-11-24 11:03:32 +0100

    completedTestedSalesRoutes

--- models/Events.js
+++ models/Events.js
@@ -1,7 +1,7 @@
 const { Schema, model } = require("mongoose");
 
 // TODO: Please make sure you edit the User model to whatever makes sense in this case
-const userSchema = new Schema(
+const eventSchema = new Schema(
     {
         creator: {
             type: Schema.Types.ObjectId,
@@ -16,13 +16,12 @@ const userSchema = new Schema(
             type: String,
             required: [true, "Type of event is required"],
             enum: ["Party", "Concert", "Join a Band", "Hiring a Member", "Jamming"
-
             ]
         },
 
         style: {
             type: String,
-            enum: ['Dance", "Folk", "Bachata", "Rock", "Reggaeton", "Rap", "Flamenco", "Classic", "Tango", "Indie","Trap", "Pop", "Electronic", "Blues", "Punk", "Jazz", "Techno", "Choir", "Trance",]
+            enum: ["Dance", "Folk", "Bachata", "Rock", "Reggaeton", "Rap", "Flamenco", "Classic", "Tango", "Indie","Trap", "Pop", "Electronic", "Blues", "Punk", "Jazz", "Techno", "Choir", "Trance",]
         },
 
         instruments: {
@@ -65,6 +64,6 @@ const userSchema = new Schema(
     }
 );
 
-const Event = model("Event", userSchema);
+const Event = model("Event", eventSchema);
 
 module.exports = Event;
